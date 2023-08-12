763efda270c65ee6e128ec1cd51c5003f2767c67
Caner Tüysüz <canertuysuz@Caners-MacBook-Pro.local>
2023-08-12 14:30:35 +0300

    image source fixed

--- src/components/no-match/index.jsx
+++ src/components/no-match/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NoMatch() {
     return (
         <>
-            <img src="../../../public/A040Lxr.png" alt="" width="300px"/>
+            <img src="https://i.imgur.com/A040Lxr.png" alt="" width="300px"/>
             <h2>404 PAGE NOT FOUND!</h2>
         </>
     )
